e76890c3d0b326d704fd74f422c0245d2a72d897
Raphael24 <raphael24@gmx.ch>
2021-10-21 21:58:38 +0200

    init

---
+++ transmit.py
@@ -0,0 +1,42 @@
+from argparse import ArgumentParser
+import os
+parser = ArgumentParser()
+
+parser.add_argument("arsch")
+args = parser.parse_args()
+
+
+def schalterAON():
+    print("schalter A ON")
+    os.system("sudo ./codesend 5506385")
+
+
+def schalterAOFF():
+    print("schalter A OFF")
+    os.system("sudo ./codesend 5506388")
+
+def schalterBON():
+    print("schalter B ON")
+    os.system("sudo ./codesend 5509457")
+
+def schalterBOFF():
+    print("schalter B OFF")
+    os.system("sudo ./codesend 5509460")
+
+
+
+if args.arsch == "AON":
+    print('schaltera')
+    schalterAON()
+
+if args.arsch == "AOFF":
+    print('schaltera')
+    schalterAOFF()
+
+if args.arsch == "BON":
+    print('schaltera')
+    schalterBON()
+
+if args.arsch == "BOFF":
+    print('schaltera')
+    schalterBOFF()
